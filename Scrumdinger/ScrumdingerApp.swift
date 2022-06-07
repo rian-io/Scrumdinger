3a266836a541a96d6b3c7428130166d5da60aebe
Rian I. de Oliveira <rian-io@users.noreply.github.com>
2022-06-07 19:26:13 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Rian Iago de Oliveira on 07/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
