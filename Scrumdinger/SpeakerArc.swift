3c6cce184aa2cf5029273f056f91eba19a85bed1
Rian I. de Oliveira <rian-io@users.noreply.github.com>
2022-06-22 13:42:21 -0300

    Drawing the Timer View

---
+++ Scrumdinger/SpeakerArc.swift
@@ -0,0 +1,32 @@
+//
+//  SpeakerArc.swift
+//  Scrumdinger
+//
+//  Created by Rian Iago de Oliveira on 22/06/22.
+//
+
+import SwiftUI
+
+struct SpeakerArc: Shape {
+    let speakerIndex: Int
+    let totalSpeakers: Int
+    
+    private var degreesPerSpeaker: Double {
+        360 / Double(totalSpeakers)
+    }
+    private var startAngle: Angle {
+        Angle(degrees: degreesPerSpeaker * Double(speakerIndex) + 1.0)
+    }
+    private var endAngle: Angle {
+        Angle(degrees: startAngle.degrees + degreesPerSpeaker - 1.0)
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let diameter = min(rect.size.width, rect.size.height) - 24.0
+        let radius = diameter / 2.0
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        return Path { path in
+            path.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+        }
+    }
+}
